47c2b93ac761e7c181957adfbe9665800d546b06
DHR60 <dehongren60@gmail.com>
2025-04-11 21:43:24 +0800

    build (self-use)
    r2r-aot
    
    dotnet 9 and x86

--- package-release-zip.sh
+++ package-release-zip.sh
@@ -4,7 +4,7 @@ Arch="$1"
 OutputPath="$2"
 
 OutputArch="v2rayN-${Arch}"
-FileName="v2rayN-${Arch}.zip"
+FileName="v2rayN-${Arch}.7z"
 
 wget -nv -O $FileName "https://github.com/2dust/v2rayN-core-bin/raw/refs/heads/master/$FileName"
 
@@ -12,4 +12,4 @@ ZipPath64="./$OutputArch"
 mkdir $ZipPath64
 
 cp -rf $OutputPath "$ZipPath64/$OutputArch"
-7z a -tZip $FileName "$ZipPath64/$OutputArch" -mx1
+7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on $FileName "$ZipPath64/$OutputArch"
