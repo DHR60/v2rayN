1b8537500299eef272838253077101accb862003
DHR60 <dehongren60@gmail.com>
2025-04-11 18:30:45 +0800

    Enables dhcp interface configuration

--- v2rayN/ServiceLib/Services/CoreConfig/CoreConfigSingboxService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/CoreConfigSingboxService.cs
@@ -1313,7 +1313,7 @@ public class CoreConfigSingboxService
         var tag = "local_local";
         var localDnsAddress = string.IsNullOrEmpty(dNSItem?.DomainDNSAddress) ? Global.SingboxDomainDNSAddress.FirstOrDefault() : dNSItem?.DomainDNSAddress;
         string? localDnsType = null;
-        //string? dhcpDnsInterface = null;
+        string? dhcpDnsInterface = null;
         if (localDnsAddress == "local")
         {
             localDnsType = "local";
@@ -1322,8 +1322,12 @@ public class CoreConfigSingboxService
         else if (localDnsAddress.StartsWith("dhcp") && localDnsAddress.Length > 7)
         {
             localDnsType = "dhcp";
-            // // dhcp://
-            // dhcpDnsInterface = localDnsAddress.Substring(7);
+            // dhcp://
+            dhcpDnsInterface = localDnsAddress.Substring(7);
+            if (dhcpDnsInterface == "auto")
+            {
+                dhcpDnsInterface = null;
+            }
             localDnsAddress = null;
         }
         else if (localDnsAddress.StartsWith("tcp") && localDnsAddress.Length > 6)
@@ -1359,7 +1363,8 @@ public class CoreConfigSingboxService
         {
             tag = tag,
             type = localDnsType,
-            server = localDnsAddress
+            server = localDnsAddress,
+            Interface = dhcpDnsInterface
         });
         dns4Sbox.rules.Insert(0, new()
         {
