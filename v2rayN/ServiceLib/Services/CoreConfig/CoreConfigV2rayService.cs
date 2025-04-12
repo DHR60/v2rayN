d8c9ce978603d66b971311c00dc63ea6807e8cad
DHR60 <dehongren60@gmail.com>
2025-04-12 12:05:47 +0800

    Fixes config generation

--- v2rayN/ServiceLib/Services/CoreConfig/CoreConfigV2rayService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/CoreConfigV2rayService.cs
@@ -1398,7 +1398,8 @@ public class CoreConfigV2rayService
                         if (prevNode is not null
                             && prevNode.ConfigType != EConfigType.Custom
                             && prevNode.ConfigType != EConfigType.Hysteria2
-                            && prevNode.ConfigType != EConfigType.TUIC)
+                            && prevNode.ConfigType != EConfigType.TUIC
+                            && prevNode.ConfigType != EConfigType.Anytls)
                         {
                             var prevOutbound = JsonUtils.Deserialize<Outbounds4Ray>(txtOutbound);
                             await GenOutbound(prevNode, prevOutbound);
@@ -1467,7 +1468,8 @@ public class CoreConfigV2rayService
             if (nextNode is not null
                 && nextNode.ConfigType != EConfigType.Custom
                 && nextNode.ConfigType != EConfigType.Hysteria2
-                && nextNode.ConfigType != EConfigType.TUIC)
+                && nextNode.ConfigType != EConfigType.TUIC
+                && nextNode.ConfigType != EConfigType.Anytls)
             {
                 if (nextOutbound == null)
                 {
