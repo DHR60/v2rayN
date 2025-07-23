f1b7b4deba20eb655a871c5c297f8a910988a36f
DHR60 <dehongren60@gmail.com>
2025-07-23 23:20:54 +0800

    Add Minimal Core Config
    Add Minimal Config Type
    Config and GUI
    Config Handler
    
    Fixes
    Enable Tun SplitCore
    
    Fixes
    
    Rename tag
    
    Fixes SplitCore == false and Node.CoreType == ECoreType.hysteria2 etc. logic
    
    Fixes (Prioritize Node.CoreType over SplitCore configuration)
    
    ShowClashUI
    
    Fixes
    
    Try add GUI support
    avalonia
    
    Fixes NaiveProxy
    
    Add Naive etc. uri share
    
    URI sharing is not compatible with other clients. Compatibility was not part of the original design and it is intended solely for internal sharing and copying within the application.
    
    Adds Xray and Singbox config type support
    Fixes hy2
    
    Fixes
    Enhances core configuration handling.
    
    Fixes Shadowquic Config Generate
    
    Refactor

--- v2rayN/ServiceLib/Enums/EConfigType.cs
+++ v2rayN/ServiceLib/Enums/EConfigType.cs
@@ -15,4 +15,9 @@ public enum EConfigType
     Anytls = 11,
     PolicyGroup = 101,
     ProxyChain = 102,
+    
+    NaiveProxy = 201,
+    Juicity = 202,
+    Brook = 203,
+    Shadowquic = 204,
 }
