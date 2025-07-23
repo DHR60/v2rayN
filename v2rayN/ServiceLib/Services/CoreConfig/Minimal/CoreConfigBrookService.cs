33190526f6a343916007a5fc7263b8d7f16e0694
DHR60 <dehongren60@gmail.com>
2025-07-23 23:20:54 +0800

    Add Minimal Core Config

---
+++ v2rayN/ServiceLib/Services/CoreConfig/Minimal/CoreConfigBrookService.cs
@@ -0,0 +1,52 @@
+using ServiceLib.Manager;
+
+namespace ServiceLib.Services.CoreConfig.Minimal;
+public class CoreConfigBrookService
+{
+    private Config _config;
+    private static readonly string _tag = "CoreConfigBrookService";
+
+    public CoreConfigBrookService(Config config)
+    {
+        _config = config;
+    }
+
+    public async Task<RetResult> GeneratePureEndpointConfig(ProfileItem node)
+    {
+        var ret = new RetResult();
+        try
+        {
+            if (node == null
+                || node.Port <= 0)
+            {
+                ret.Msg = ResUI.CheckServerSettings;
+                return ret;
+            }
+
+            //if (node.ConfigType != EConfigType.Brook)
+            //{
+            //    ret.Msg = ResUI.Incorrectconfiguration + $" - {node.ConfigType}";
+            //    return ret;
+            //}
+
+            var processArgs = "client";
+
+            // inbound
+            processArgs += " --socks5 " + Global.Loopback + ":" + AppManager.Instance.GetLocalPort(EInboundProtocol.split).ToString();
+
+            // outbound
+            processArgs += " --server " + node.Address + ":" + node.Port;
+            processArgs += " --password " + node.Id;
+
+            ret.Data = processArgs;
+
+            return await Task.FromResult(ret);
+        }
+        catch (Exception ex)
+        {
+            Logging.SaveLog(_tag, ex);
+            ret.Msg = ResUI.FailedGenDefaultConfiguration;
+            return await Task.FromResult(ret);
+        }
+    }
+}
