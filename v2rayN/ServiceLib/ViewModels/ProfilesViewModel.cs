74877ac6bf0493fd70b9c863892adab49ceacbe6
DHR60 <dehongren60@gmail.com>
2025-07-23 23:20:54 +0800

    Add Minimal Core Config
    Add Minimal Config Type
    Config and GUI
    Config Handler
    
    Fixes
    Enable Tun SplitCore
    
    Fixes
    
    Rename tag
    
    Fixes SplitCore == false and Node.CoreType == ECoreType.hysteria2 etc. logic
    
    Fixes (Prioritize Node.CoreType over SplitCore configuration)
    
    ShowClashUI
    
    Fixes
    
    Try add GUI support
    avalonia
    
    Fixes NaiveProxy
    
    Add Naive etc. uri share
    
    URI sharing is not compatible with other clients. Compatibility was not part of the original design and it is intended solely for internal sharing and copying within the application.
    
    Adds Xray and Singbox config type support
    Fixes hy2
    
    Fixes
    Enhances core configuration handling.
    
    Fixes Shadowquic Config Generate
    
    Refactor
    Add group support
    
    Fix missing hysteria2 arguments
    
    Fix hy2 custom config
    
    Adds overtls and mieru support
    Adds core assignment by config type

--- v2rayN/ServiceLib/ViewModels/ProfilesViewModel.cs
+++ v2rayN/ServiceLib/ViewModels/ProfilesViewModel.cs
@@ -811,7 +811,8 @@ public class ProfilesViewModel : MyReactiveObject
 
         if (blClipboard)
         {
-            var result = await CoreConfigHandler.GenerateClientConfig(item, null);
+            var coreLaunchContext = new CoreLaunchContext(item, _config).AdjustForConfigType();
+            var result = await CoreConfigHandler.GenerateClientConfig(coreLaunchContext, null);
             if (result.Success != true)
             {
                 NoticeManager.Instance.Enqueue(result.Msg);
@@ -834,7 +835,8 @@ public class ProfilesViewModel : MyReactiveObject
         {
             return;
         }
-        var result = await CoreConfigHandler.GenerateClientConfig(item, fileName);
+        var coreLaunchContext = new CoreLaunchContext(item, _config).AdjustForConfigType();
+        var result = await CoreConfigHandler.GenerateClientConfig(coreLaunchContext, fileName);
         if (result.Success != true)
         {
             NoticeManager.Instance.Enqueue(result.Msg);
