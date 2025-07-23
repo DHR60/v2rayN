3f3fc4aa77a9fdcee076171806d3cdfe589e6b47
DHR60 <dehongren60@gmail.com>
2025-07-23 23:20:54 +0800

    Add Minimal Core Config
    Add Minimal Config Type
    Config and GUI
    Config Handler
    
    Fixes
    Enable Tun SplitCore
    
    Fixes
    
    Rename tag
    
    Fixes SplitCore == false and Node.CoreType == ECoreType.hysteria2 etc. logic
    
    Fixes (Prioritize Node.CoreType over SplitCore configuration)
    
    ShowClashUI
    
    Fixes
    
    Try add GUI support
    avalonia
    
    Fixes NaiveProxy
    
    Add Naive etc. uri share
    
    URI sharing is not compatible with other clients. Compatibility was not part of the original design and it is intended solely for internal sharing and copying within the application.
    
    Adds Xray and Singbox config type support
    Fixes hy2
    
    Fixes
    Enhances core configuration handling.
    
    Fixes Shadowquic Config Generate
    
    Refactor
    Add group support
    
    Fix missing hysteria2 arguments
    
    Fix hy2 custom config
    
    Adds overtls and mieru support
    Adds core assignment by config type

---
+++ v2rayN/ServiceLib/Services/CoreConfig/Minimal/CoreConfigBrookService.cs
@@ -0,0 +1,50 @@
+using ServiceLib.Manager;
+
+namespace ServiceLib.Services.CoreConfig.Minimal;
+public class CoreConfigBrookService(Config config) : CoreConfigServiceMinimalBase(config)
+{
+    protected override async Task<RetResult> GeneratePassthroughConfig(ProfileItem node, int port)
+    {
+        var ret = new RetResult();
+        try
+        {
+            if (node == null
+                || node.Port <= 0)
+            {
+                ret.Msg = ResUI.CheckServerSettings;
+                return ret;
+            }
+
+            if (node.ConfigType != EConfigType.Brook)
+            {
+                ret.Msg = ResUI.Incorrectconfiguration + $" - {node.ConfigType}";
+                return ret;
+            }
+
+            var processArgs = "client";
+
+            // inbound
+            processArgs += " --socks5 " + Global.Loopback + ":" + port.ToString();
+
+            // outbound
+            var address = node.Address;
+            if (Utils.IsIpv6(address))
+            {
+                address = $"[{address}]";
+            }
+            processArgs += " --server " + address + ":" + node.Port;
+            processArgs += " --password " + node.Id;
+
+            ret.Success = true;
+            ret.Data = processArgs;
+
+            return await Task.FromResult(ret);
+        }
+        catch (Exception ex)
+        {
+            Logging.SaveLog(_tag, ex);
+            ret.Msg = ResUI.FailedGenDefaultConfiguration;
+            return await Task.FromResult(ret);
+        }
+    }
+}
