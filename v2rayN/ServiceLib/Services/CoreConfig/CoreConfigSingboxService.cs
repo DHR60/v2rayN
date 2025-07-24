ee890bd8b19220b8b40a7faae6d193e1d041211d
DHR60 <dehongren60@gmail.com>
2025-07-24 12:51:17 +0800

    Fixes

--- v2rayN/ServiceLib/Services/CoreConfig/CoreConfigSingboxService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/CoreConfigSingboxService.cs
@@ -599,7 +599,7 @@ public class CoreConfigSingboxService
 
             config.Remove("route");
 
-            ret.Data = config.ToJsonString(new() { WriteIndented = true });
+            ret.Data = JsonUtils.Serialize(config, true);
 
             return ret;
         }
