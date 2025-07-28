a0302abc1ee6ce19e7e4f6a34aa3f36487024561
DHR60 <dehongren60@gmail.com>
2025-07-28 23:59:58 +0800

    ntp udp test
    batch ntp udp test
    
    Avalonia support

--- v2rayN/ServiceLib/Services/CoreConfig/V2ray/CoreConfigV2rayService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/V2ray/CoreConfigV2rayService.cs
@@ -458,6 +458,11 @@ public partial class CoreConfigV2rayService(Config config) : CoreConfigServiceBa
                     listen = Global.Loopback,
                     port = port,
                     protocol = EInboundProtocol.mixed.ToString(),
+                    settings = new Inboundsettings4Ray()
+                    {
+                        udp = true,
+                        auth = "noauth"
+                    },
                 };
                 inbound.tag = inbound.protocol + inbound.port.ToString();
                 v2rayConfig.inbounds.Add(inbound);
@@ -533,6 +538,11 @@ public partial class CoreConfigV2rayService(Config config) : CoreConfigServiceBa
                 listen = Global.Loopback,
                 port = port,
                 protocol = EInboundProtocol.mixed.ToString(),
+                settings = new Inboundsettings4Ray()
+                {
+                    udp = true,
+                    auth = "noauth"
+                },
             });
 
             ret.Msg = string.Format(ResUI.SuccessfulConfiguration, "");
