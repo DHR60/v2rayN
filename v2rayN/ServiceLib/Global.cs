ec98b342e9c141bde857b9e5b86b8095b7c51970
DHR60 <dehongren60@gmail.com>
2025-07-28 23:28:20 +0800

    Add Naive etc. uri share
    URI sharing is not compatible with other clients. Compatibility was not part of the original design and it is intended solely for internal sharing and copying within the application.

--- v2rayN/ServiceLib/Global.cs
+++ v2rayN/ServiceLib/Global.cs
@@ -183,7 +183,11 @@ public class Global
             { EConfigType.Hysteria2, "hysteria2://" },
             { EConfigType.TUIC, "tuic://" },
             { EConfigType.WireGuard, "wireguard://" },
-            { EConfigType.Anytls, "anytls://" }
+            { EConfigType.Anytls, "anytls://" },
+            { EConfigType.NaiveProxy, "naive://" },
+            { EConfigType.Juicity, "juicity://" },
+            { EConfigType.Brook, "brook://" },
+            { EConfigType.Shadowquic, "shadowquic://" }
         };
 
     public static readonly Dictionary<EConfigType, string> ProtocolTypes = new()
@@ -197,7 +201,11 @@ public class Global
             { EConfigType.Hysteria2, "hysteria2" },
             { EConfigType.TUIC, "tuic" },
             { EConfigType.WireGuard, "wireguard" },
-            { EConfigType.Anytls, "anytls" }
+            { EConfigType.Anytls, "anytls" },
+            { EConfigType.NaiveProxy, "naiveproxy" },
+            { EConfigType.Juicity, "juicity" },
+            { EConfigType.Brook, "brook" },
+            { EConfigType.Shadowquic, "shadowquic" }
         };
 
     public static readonly List<string> VmessSecurities =
