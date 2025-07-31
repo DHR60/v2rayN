866d2744b2c288023405ba3509e96e46fde7d67f
DHR60 <dehongren60@gmail.com>
2025-07-31 10:46:01 +0800

    Refactor

--- v2rayN/ServiceLib/ViewModels/ProfilesViewModel.cs
+++ v2rayN/ServiceLib/ViewModels/ProfilesViewModel.cs
@@ -761,7 +761,8 @@ public class ProfilesViewModel : MyReactiveObject
         }
         if (blClipboard)
         {
-            var result = await CoreConfigHandler.GenerateClientConfig(item, null);
+            var coreLaunchContext = new CoreLaunchContext(item, _config);
+            var result = await CoreConfigHandler.GenerateClientConfig(coreLaunchContext, null);
             if (result.Success != true)
             {
                 NoticeManager.Instance.Enqueue(result.Msg);
@@ -784,7 +785,8 @@ public class ProfilesViewModel : MyReactiveObject
         {
             return;
         }
-        var result = await CoreConfigHandler.GenerateClientConfig(item, fileName);
+        var coreLaunchContext = new CoreLaunchContext(item, _config);
+        var result = await CoreConfigHandler.GenerateClientConfig(coreLaunchContext, fileName);
         if (result.Success != true)
         {
             NoticeManager.Instance.Enqueue(result.Msg);
