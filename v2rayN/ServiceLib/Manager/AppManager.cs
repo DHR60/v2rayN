2643c9406292f53384755f2f1c2182a9dc36622a
DHR60 <dehongren60@gmail.com>
2025-08-01 11:30:07 +0800

    Fix custom config core type not working

--- v2rayN/ServiceLib/Manager/AppManager.cs
+++ v2rayN/ServiceLib/Manager/AppManager.cs
@@ -242,6 +242,11 @@ public sealed class AppManager
             return (ECoreType)profileItem.CoreType;
         }
 
+        return GetCoreType(eConfigType);
+    }
+
+    public ECoreType GetCoreType(EConfigType eConfigType)
+    {
         var item = _config.CoreTypeItem?.FirstOrDefault(it => it.ConfigType == eConfigType);
         return item?.CoreType ?? ECoreType.Xray;
     }
@@ -253,6 +258,11 @@ public sealed class AppManager
             return (ECoreType)profileItem.CoreType;
         }
 
+        return GetSplitCoreType(eConfigType);
+    }
+
+    public ECoreType GetSplitCoreType(EConfigType eConfigType)
+    {
         var item = _config.SplitCoreItem.SplitCoreTypes?.FirstOrDefault(it => it.ConfigType == eConfigType);
         return item?.CoreType ?? ECoreType.Xray;
     }
@@ -273,7 +283,7 @@ public sealed class AppManager
             coreType = profileItem.CoreType ?? ECoreType.Xray;
             if (profileItem.PreSocksPort > 0)
             {
-                preCoreType = enableTun ? ECoreType.sing_box : GetCoreType(profileItem, profileItem.ConfigType);
+                preCoreType = enableTun ? ECoreType.sing_box : GetCoreType(profileItem.ConfigType);
             }
             else
             {
