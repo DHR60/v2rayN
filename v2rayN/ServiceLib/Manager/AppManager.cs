352490c16571475c3c1e86dc54c1059eb2f1c10e
DHR60 <dehongren60@gmail.com>
2025-09-06 20:12:04 +0800

    Multi Profile

--- v2rayN/ServiceLib/Manager/AppManager.cs
+++ v2rayN/ServiceLib/Manager/AppManager.cs
@@ -67,6 +67,7 @@ public sealed class AppManager
         SQLiteHelper.Instance.CreateTable<ProfileExItem>();
         SQLiteHelper.Instance.CreateTable<DNSItem>();
         SQLiteHelper.Instance.CreateTable<FullConfigTemplateItem>();
+        SQLiteHelper.Instance.CreateTable<ProfileGroupItem>();
         return true;
     }
 
@@ -101,6 +102,7 @@ public sealed class AppManager
 
             await ConfigHandler.SaveConfig(_config);
             await ProfileExManager.Instance.SaveTo();
+            await ProfileGroupItemManager.Instance.SaveTo();
             await StatisticsManager.Instance.SaveTo();
             await CoreManager.Instance.CoreStop();
             StatisticsManager.Instance.Close();
@@ -217,6 +219,15 @@ public sealed class AppManager
             return null;
         }
         return await SQLiteHelper.Instance.TableAsync<ProfileItem>().FirstOrDefaultAsync(it => it.Remarks == remarks);
+    }
+
+    public async Task<ProfileGroupItem?> GetProfileGroupItem(string parentIndexId)
+    {
+        if (parentIndexId.IsNullOrEmpty())
+        {
+            return null;
+        }
+        return await SQLiteHelper.Instance.TableAsync<ProfileGroupItem>().FirstOrDefaultAsync(it => it.ParentIndexId == parentIndexId);
     }
 
     public async Task<List<RoutingItem>?> RoutingItems()
