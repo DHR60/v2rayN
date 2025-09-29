d0c82ae0aadbdc6ec7c8349b04ac1445f1559a49
DHR60 <192860629+DHR60@users.noreply.github.com>
2025-09-29 17:18:25 +0800

    try fix

--- v2rayN/v2rayN.Desktop/Views/MsgView.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MsgView.axaml.cs
@@ -33,6 +33,13 @@ public partial class MsgView : ReactiveUserControl<MsgViewModel>
 
     private void ShowMsg(object msg)
     {
+        // try avoid non ui thread call
+        if (!Dispatcher.UIThread.CheckAccess())
+        {
+            Dispatcher.UIThread.Post(() => ShowMsg(msg));
+            return;
+        }
+
         //var lineCount = txtMsg.LineCount;
         //if (lineCount > ViewModel?.NumMaxMsg)
         //{
@@ -53,6 +60,13 @@ public partial class MsgView : ReactiveUserControl<MsgViewModel>
 
     public void ClearMsg()
     {
+        // try avoid non ui thread call
+        if (!Dispatcher.UIThread.CheckAccess())
+        {
+            Dispatcher.UIThread.Post(ClearMsg);
+            return;
+        }
+
         txtMsg.Clear();
         txtMsg.AppendText("----- Message cleared -----\n");
     }
