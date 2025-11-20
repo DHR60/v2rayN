22bad17c7883c738673b3ad16b531cdbf0aeadf5
DHR60 <dehongren60@gmail.com>
2025-11-20 20:37:59 +0800

    build (self-use)
    r2r-aot
    
    dotnet 10 and x86

--- v2rayN/ServiceLib/Common/JsonUtils.cs
+++ v2rayN/ServiceLib/Common/JsonUtils.cs
@@ -58,6 +58,7 @@ public class JsonUtils
             {
                 return default;
             }
+            EnsureResolver(_defaultDeserializeOptions);
             return JsonSerializer.Deserialize<T>(strJson, _defaultDeserializeOptions);
         }
         catch
@@ -105,6 +106,7 @@ public class JsonUtils
                 return result;
             }
             var options = nullValue ? _nullValueSerializeOptions : _defaultSerializeOptions;
+            EnsureResolver(options);
             result = JsonSerializer.Serialize(obj, options);
         }
         catch (Exception ex)
@@ -129,7 +131,9 @@ public class JsonUtils
             {
                 return result;
             }
-            result = JsonSerializer.Serialize(obj, options ?? _defaultSerializeOptions);
+            options ??= _defaultSerializeOptions;
+            EnsureResolver(options);
+            result = JsonSerializer.Serialize(obj, options);
         }
         catch (Exception ex)
         {
@@ -145,6 +149,21 @@ public class JsonUtils
     /// <returns></returns>
     public static JsonNode? SerializeToNode(object? obj, JsonSerializerOptions? options = null)
     {
+        if (options is null)
+        {
+            options = new JsonSerializerOptions();
+        }
+        EnsureResolver(options);
         return JsonSerializer.SerializeToNode(obj, options);
+    }
+
+    private static void EnsureResolver(JsonSerializerOptions options)
+    {
+        // In trimmed/AOT builds, reflection-based serialization is disabled by default.
+        // Opt-in explicitly so generic helpers continue to work.
+        if (options.TypeInfoResolverChain.Count == 0)
+        {
+            options.TypeInfoResolverChain.Add(new DefaultJsonTypeInfoResolver());
+        }
     }
 }
