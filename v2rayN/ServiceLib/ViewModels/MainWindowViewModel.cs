4bedc64fd7293c9ede2001b5c0a128644898a1fc
DHR60 <192860629+DHR60@users.noreply.github.com>
2025-12-20 12:42:17 +0800

    Add fork description

--- v2rayN/ServiceLib/ViewModels/MainWindowViewModel.cs
+++ v2rayN/ServiceLib/ViewModels/MainWindowViewModel.cs
@@ -660,6 +660,9 @@ public partial class MainWindowViewModel : MyReactiveObject
                 return;
             }
 
+            // add fork description
+            NoticeManager.Instance.SendMessage("This is a forked version.");
+
             await Task.Run(async () =>
             {
                 await LoadCore(allResult.MainResult.Context, allResult.PreSocksResult?.Context);
