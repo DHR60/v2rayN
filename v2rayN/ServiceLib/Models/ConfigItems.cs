6cfe8e9b54cdcf7e002bcce8fe6062d63003a40a
DHR60 <dehongren60@gmail.com>
2026-02-11 16:24:15 +0800

    Add Test Result Divisor

--- v2rayN/ServiceLib/Models/ConfigItems.cs
+++ v2rayN/ServiceLib/Models/ConfigItems.cs
@@ -157,6 +157,7 @@ public class SpeedTestItem
     public int MixedConcurrencyCount { get; set; }
     public string IPAPIUrl { get; set; }
     public string UdpTestTarget { get; set; }
+    public string TestResultDivisor { get; set; } = "1";
 }
 
 [Serializable]
