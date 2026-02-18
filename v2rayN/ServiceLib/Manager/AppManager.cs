54b64e42fc0d035b3c76fbcea17183ba53065e81
DHR60 <dehongren60@gmail.com>
2026-02-18 22:28:43 +0800

    Add UoT support

--- v2rayN/ServiceLib/Manager/AppManager.cs
+++ v2rayN/ServiceLib/Manager/AppManager.cs
@@ -287,11 +287,10 @@ public sealed class AppManager
         return await SQLiteHelper.Instance.TableAsync<FullConfigTemplateItem>().FirstOrDefaultAsync(it => it.CoreType == eCoreType);
     }
 
+#pragma warning disable CS0618
     public async Task MigrateProfileExtra()
     {
         await MigrateProfileExtraGroup();
-
-#pragma warning disable CS0618
 
         const int pageSize = 100;
         var offset = 0;
@@ -316,7 +315,6 @@ public sealed class AppManager
         }
 
         //await ProfileGroupItemManager.Instance.ClearAll();
-#pragma warning restore CS0618
     }
 
     private async Task<int> MigrateProfileExtraSub(List<ProfileItem> batch)
@@ -362,6 +360,7 @@ public sealed class AppManager
                         break;
 
                     case EConfigType.TUIC:
+                        extra = extra with { CongestionControl = item.HeaderType.NullIfEmpty(), };
                         item.Username = item.Id;
                         item.Id = item.Security;
                         item.Password = item.Security;
@@ -418,7 +417,6 @@ public sealed class AppManager
 
     private async Task<bool> MigrateProfileExtraGroup()
     {
-#pragma warning disable CS0618
         var list = await SQLiteHelper.Instance.TableAsync<ProfileGroupItem>().ToListAsync();
         var groupItems = new ConcurrentDictionary<string, ProfileGroupItem>(list.Where(t => !string.IsNullOrEmpty(t.IndexId)).ToDictionary(t => t.IndexId!));
 
@@ -483,8 +481,8 @@ public sealed class AppManager
         return true;
 
         //await ProfileGroupItemManager.Instance.ClearAll();
-#pragma warning restore CS0618
     }
+#pragma warning restore CS0618
 
     #endregion SqliteHelper
 
