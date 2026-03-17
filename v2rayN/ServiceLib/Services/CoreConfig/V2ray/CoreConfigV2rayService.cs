b9f965b603c103175836ca4298cf5b74f3b6eb28
DHR60 <dehongren60@gmail.com>
2026-03-17 23:30:10 +0800

    UDP Test
    Increases UDP test timeout
    
    Pref exception
    
    Fix
    
    Add Minecraft Bedrock Edition Test
    
    Optimization

--- v2rayN/ServiceLib/Services/CoreConfig/V2ray/CoreConfigV2rayService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/V2ray/CoreConfigV2rayService.cs
@@ -163,6 +163,11 @@ public partial class CoreConfigV2rayService(CoreConfigContext context)
                     listen = Global.Loopback,
                     port = port,
                     protocol = EInboundProtocol.mixed.ToString(),
+                    settings = new Inboundsettings4Ray()
+                    {
+                        udp = true,
+                        auth = "noauth"
+                    },
                 };
                 inbound.tag = inbound.protocol + inbound.port.ToString();
                 _coreConfig.inbounds.Add(inbound);
@@ -254,6 +259,11 @@ public partial class CoreConfigV2rayService(CoreConfigContext context)
                 listen = Global.Loopback,
                 port = port,
                 protocol = EInboundProtocol.mixed.ToString(),
+                settings = new Inboundsettings4Ray()
+                {
+                    udp = true,
+                    auth = "noauth"
+                },
             });
 
             _coreConfig.routing.rules.Add(BuildFinalRule());
