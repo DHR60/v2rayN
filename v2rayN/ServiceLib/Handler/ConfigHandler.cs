0db2b97deacccaac646f68618854f46580ed074b
DHR60 <dehongren60@gmail.com>
2026-03-17 23:30:10 +0800

    UDP Test
    Increases UDP test timeout
    
    Pref exception
    
    Fix
    
    Add Minecraft Bedrock Edition Test
    
    Optimization

--- v2rayN/ServiceLib/Handler/ConfigHandler.cs
+++ v2rayN/ServiceLib/Handler/ConfigHandler.cs
@@ -133,6 +133,10 @@ public static class ConfigHandler
         {
             config.SpeedTestItem.MixedConcurrencyCount = 5;
         }
+        if (config.SpeedTestItem.UdpTestTarget.IsNullOrEmpty())
+        {
+            config.SpeedTestItem.UdpTestTarget = Global.UdpTestTargets.First();
+        }
 
         config.Mux4RayItem ??= new()
         {
--- v2rayN/ServiceLib/Services/CoreConfig/V2ray/CoreConfigV2rayService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/V2ray/CoreConfigV2rayService.cs
@@ -162,6 +162,11 @@ public partial class CoreConfigV2rayService(CoreConfigContext context)
                     listen = Global.Loopback,
                     port = port,
                     protocol = EInboundProtocol.mixed.ToString(),
+                    settings = new Inboundsettings4Ray()
+                    {
+                        udp = true,
+                        auth = "noauth"
+                    },
                 };
                 inbound.tag = inbound.protocol + inbound.port.ToString();
                 _coreConfig.inbounds.Add(inbound);
@@ -252,6 +257,11 @@ public partial class CoreConfigV2rayService(CoreConfigContext context)
                 listen = Global.Loopback,
                 port = port,
                 protocol = EInboundProtocol.mixed.ToString(),
+                settings = new Inboundsettings4Ray()
+                {
+                    udp = true,
+                    auth = "noauth"
+                },
             });
 
             _coreConfig.routing.rules.Add(BuildFinalRule());
