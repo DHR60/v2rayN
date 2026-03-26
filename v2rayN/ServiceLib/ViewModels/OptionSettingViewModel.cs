1d393fdc48ee3dfde0869a9c3548709c56a7ff1b
DHR60 <dehongren60@gmail.com>
2026-03-26 20:42:09 +0800

    Add Test Result Divisor

--- v2rayN/ServiceLib/ViewModels/OptionSettingViewModel.cs
+++ v2rayN/ServiceLib/ViewModels/OptionSettingViewModel.cs
@@ -61,6 +61,7 @@ public class OptionSettingViewModel : MyReactiveObject
     [Reactive] public string SpeedTestUrl { get; set; }
     [Reactive] public string SpeedPingTestUrl { get; set; }
     [Reactive] public string UdpTestTarget { get; set; }
+    [Reactive] public string TestResultDivisor { get; set; }
     [Reactive] public int MixedConcurrencyCount { get; set; }
     [Reactive] public bool EnableHWA { get; set; }
     [Reactive] public string SubConvertUrl { get; set; }
@@ -200,6 +201,7 @@ public class OptionSettingViewModel : MyReactiveObject
         MixedConcurrencyCount = _config.SpeedTestItem.MixedConcurrencyCount;
         SpeedPingTestUrl = _config.SpeedTestItem.SpeedPingTestUrl;
         UdpTestTarget = _config.SpeedTestItem.UdpTestTarget;
+        TestResultDivisor = _config.SpeedTestItem.TestResultDivisor;
         EnableHWA = _config.GuiItem.EnableHWA;
         SubConvertUrl = _config.ConstItem.SubConvertUrl;
         MainGirdOrientation = (int)_config.UiItem.MainGirdOrientation;
@@ -376,6 +378,7 @@ public class OptionSettingViewModel : MyReactiveObject
         _config.SpeedTestItem.SpeedTestUrl = SpeedTestUrl;
         _config.SpeedTestItem.SpeedPingTestUrl = SpeedPingTestUrl;
         _config.SpeedTestItem.UdpTestTarget = UdpTestTarget;
+        _config.SpeedTestItem.TestResultDivisor = TestResultDivisor;
         _config.GuiItem.EnableHWA = EnableHWA;
         _config.ConstItem.SubConvertUrl = SubConvertUrl;
         _config.UiItem.MainGirdOrientation = (EGirdOrientation)MainGirdOrientation;
