d580c6c102c10d749ee8d82d3432a542facf092f
DHR60 <192860629+DHR60@users.noreply.github.com>
2026-07-13 00:24:37 +0800

    Support sing-box 1.14
    Unified format
    
    Support parallel dns
    
    Add `optimistic DNS caching` support
    
    Try fix serial multi DNS

--- v2rayN/ServiceLib/Models/CoreConfigs/SingboxConfig.cs
+++ v2rayN/ServiceLib/Models/CoreConfigs/SingboxConfig.cs
@@ -24,6 +24,7 @@ public class Dns4Sbox
     public List<Server4Sbox> servers { get; set; }
     public List<Rule4Sbox> rules { get; set; }
     public string? final { get; set; }
+    public bool? optimistic { get; set; }
     public string? strategy { get; set; }
     public bool? disable_cache { get; set; }
     public bool? disable_expire { get; set; }
@@ -94,6 +95,11 @@ public class Rule4Sbox
     public bool? rule_set_ip_cidr_match_source { get; set; }
     public bool? rule_set_ip_cidr_accept_empty { get; set; }
     public bool? tls_record_fragment { get; set; }
+    public string? preferred_by { get; set; }
+    public string? match_response { get; set; } // or bool
+    public string? tag { get; set; }
+    public bool? race { get; set; }
+    public bool? speculative { get; set; }
 }
 
 [Serializable]
@@ -108,7 +114,6 @@ public class Inbound4Sbox
     public int? mtu { get; set; }
     public bool? auto_route { get; set; }
     public bool? strict_route { get; set; }
-    public bool? endpoint_independent_nat { get; set; }
     public string? stack { get; set; }
     public List<User4Sbox> users { get; set; }
     public List<string>? route_exclude_address { get; set; }
